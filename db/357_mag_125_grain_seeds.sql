6cb97ec556ed8966cdfb9d6f829d2a21e55e0bfd
Max Power <dbroncosfan@gmail.com>
2020-07-12 00:42:07 -0600

    initial commit

---
+++ db/357_mag_125_grain_seeds.sql
@@ -0,0 +1,49 @@
+USE reloading;
+INSERT INTO 357_mag_125_grain (caliber, bullet_grain, propellant, start_grains, start_velocity, max_grains, max_velocity)
+VALUES (
+	"357 Magnum",
+    "125 Grain",
+    "Power Pistol",
+    "9.5",
+    "1273",
+    "10.5",
+    "1345"
+),
+(
+	"357 Magnum",
+    "125 Grain",
+    "Unique",
+    "8.6",
+    "1259",
+    "9.6",
+    "1343"
+),
+(
+	"357 Magnum",
+    "125 Grain",
+    "296",
+    "18.3",
+    "1188",
+    "20.3",
+    "1336"
+),
+(
+	"357 Magnum",
+    "125 Grain",
+    "H110",
+    "18.0",
+    "1154",
+    "20.0",
+    "1282"
+),
+(
+	"357 Magnum",
+    "125 Grain",
+    "AA No. 9",
+    "12.6",
+    "1119",
+    "14.6",
+    "1238"
+);
+
+SELECT * FROM 357_mag_125_grain;
